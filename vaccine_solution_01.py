0c8bfb99a326bc5e5137e0e2b0abcdb314f54c6d
Abraham Ibanez <piol_4444@hotmail.com>
2021-03-03 19:48:32 -0800

    Update vaccine_solution_01.py

--- vaccine_solution_01.py
+++ vaccine_solution_01.py
@@ -1,1 +1,11 @@
+# 1. Check for any vaccine.py and delete if any.
+# 2. Verify that the userSetup.py is not infected, if so rename to userSetup.backup and notify user (email)
+# 3. Change ~/maya/script/ folder permissions to read only using os.chmod ? 
+# 4. Create scriptJob:
 
+def restore_folder_permissions():
+   # Delete unwanted scriptNodes
+   # Restore ~/maya/script/ folder permissions using os.chmod?
+   # remove the script node -> cmds.scriptJob(kill=temp_script_job)
+
+temp_script_job = cmds.scriptJob(e=("SceneOpened", remove_unwanted_script_nodes))
